43c75670a2a2b5946accae3f714c51ba506b73a2
unknown <carlos2000sistemasc@gmail.com>
2023-03-19 00:08:18 -0700

    calculadora con vue normal

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Calculadora />
+</template>
+
+<script>
+import Calculadora from "@/components/Calculadora.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+}
+</style>
